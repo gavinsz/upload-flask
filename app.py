6c3e97384df3019ba4e7d944282703fb489b7735
gavin <freeswitch@126.com>
2019-07-13 11:22:22 +0800

    added print args

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, request, url_for, send_from_directory
+from flask import Flask, request, url_for, send_from_directory, render_template, redirect
 from werkzeug import secure_filename
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -13,13 +13,33 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 html = '''
     <!DOCTYPE html>
     <title>Upload File</title>
-    <h1>Photo Upload</h1>
+    <h1>file upload</h1>
     <form method=post enctype=multipart/form-data>
          <input type=file name=file>
          <input type=submit value=upload>
     </form>
     '''
 
+print_args = '''
+<form method=post>
+    颜色:
+    <select name="color_mode">
+        <option value="color">color</option>
+        <option value="black">black</option>
+    </select>
+    单双面:
+    <select name="sides">
+        <option value="one-side">one</option>
+        <option value="two-sides">two</option>
+    </select>
+    打印份数:
+    <input type="text" name="copys" value="1">
+    <input type="text" name="file_url" value="http://192.168.0.111:5002/uploads/hi.doc.pdf">
+    <br><br>
+    <input type="submit" value="打印">
+</form> 
+
+    '''
 
 def allowed_file(filename):
     return True
@@ -36,6 +56,10 @@ def uploaded_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+        color_mode = request.form.get('color_mode')
+        if None != color_mode:
+            return print_req()
+
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -45,10 +69,27 @@ def upload_file():
             ret = os.popen(cmd).readlines()
             print('exec cmd %s|ret=%s'%(cmd, ret))
             file_url = url_for('uploaded_file', filename=pdf_file)
-            return html + '<embed src=\"{}\" type=\"application/pdf\"   height=\"800px\" width=\"100%\">'.format(file_url)
-            #return html + '<br><img src=' + file_url + '>'
+            #return html + '<embed src=\"{}\" type=\"application/pdf\"   height=\"800px\" width=\"100%\">'.format(file_url)
+            page = html + '<embed src=\"{}\" type=\"application/pdf\"   height=\"300px\" width=\"100%\">'.format(file_url)
+            page = page + print_args
+            return page
+
     return html
 
+#@app.route('/print_req', methods=['post'])
+def print_req():
+    if request.method == 'POST':
+        color_mode = request.form.get('color_mode')
+        if color_mode:
+            sides = request.form.get('sides')
+            copys = request.form.get('copys')
+            file_url = request.form.get('file_url')
+            print('file_url=%s|color_mode=%s|sides=%s|copys=%s'%(file_url, color_mode, sides, copys))
+            return '打印任务提交成功！'
+        else:
+            return '打印参数错误！'
+
+    return '非法访问！'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
