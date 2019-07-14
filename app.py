4d336edded2c93a0e74c0b47e2778db619039b83
gavin <freeswitch@126.com>
2019-07-14 15:57:37 +0800

    modify html

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import os
 import uuid
 import calendar,time
 import redis
+import socket
 from flask import Flask, request, url_for, send_from_directory, render_template, redirect
 from werkzeug import secure_filename
+from pip._internal.download import is_file_url
 #from flask_redis import FlaskRedis
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 REDIS_URL = "redis://:password@localhost:6379/0"
+download_url = ''
+listening_port = 5002
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.getcwd()
@@ -30,21 +34,31 @@ print_args = '''
 <form method=post>
     颜色:
     <select name="color_mode">
-        <option value="color">color</option>
-        <option value="black">black</option>
+        <option value="color">彩色</option>
+        <option value="black">黑白</option>
     </select>
-    单双面:
+    &nbsp;&nbsp;单双面:
     <select name="sides">
-        <option value="one-side">one</option>
-        <option value="two-sides">two</option>
+        <option value="one">单面</option>
+        <option value="two-sided-long-edge">双面长边</option>
+        <option value="two-sided-short-edgee">双面短边</option>
+        
     </select>
-    打印份数:
+    &nbsp;&nbsp;打印份数:
     <input type="text" name="copys" value="1">
-    <input type="text" name="file_url" value="http://192.168.0.111:5002/uploads/hi.doc.pdf">
+    <input type="text" id="file_url" name="file_url" value="" style="display:none">
     <br><br>
     <input type="submit" value="打印">
-</form> 
-
+</form>
+<script>
+function set_file_url(){
+var embed=document.getElementById("embed_pdf");
+var file_name = embed.getAttribute("src");
+var file_url = document.getElementById("file_url");
+file_url.value= file_name;
+}
+set_file_url();
+</script>
     '''
 
 def allowed_file(filename):
@@ -76,7 +90,7 @@ def upload_file():
             print('exec cmd %s|ret=%s'%(cmd, ret))
             file_url = url_for('uploaded_file', filename=pdf_file)
             #return html + '<embed src=\"{}\" type=\"application/pdf\"   height=\"800px\" width=\"100%\">'.format(file_url)
-            page = html + '<embed src=\"{}\" type=\"application/pdf\"   height=\"768px\" width=\"100%\">'.format(file_url)
+            page = html + '<embed id=\"embed_pdf\" src=\"{}\" type=\"application/pdf\"   height=\"768px\" width=\"100%\">'.format(file_url)
             page = page + print_args
             return page
 
@@ -89,9 +103,11 @@ def print_req():
         if color_mode:
             sides = request.form.get('sides')
             copys = request.form.get('copys')
-            file_url = request.form.get('file_url')
+            file = request.form.get('file_url')
+            download_url = 'http://{}:{}{}'.format(get_host_ip(), listening_port, file)
+            file_url = download_url
             
-            print_args = 'color_mode:{},sides:{},copys:{}'.format(color_mode, sides, copys)
+            print_args = '{},{},{}'.format(color_mode, sides, copys)
             request_id = str(uuid.uuid4())
             dev_id = 'a46fd75c-a5e0-11e9-86f1-20689d49592c'
             timestamp = calendar.timegm(time.gmtime())
@@ -99,8 +115,7 @@ def print_req():
             #store print request info
             try:
                 r.hmset(request_id, {'file_url':file_url, \
-                                            'color_mode':color_mode, \
-                                            'sides':sides, \
+                                            'print_args':print_args, \
                                             'copys':copys, \
                                             'dev_id':dev_id, \
                                             'timestamp':timestamp})
@@ -120,5 +135,14 @@ def print_req():
 
     return '非法访问！'
 
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=listening_port)
