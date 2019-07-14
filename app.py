1f04f3bac7f2bb088fabafcc962b2347c5ed53ae
gavin <freeswitch@126.com>
2019-07-14 14:26:00 +0800

    using python redis replace flask_redis

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 import os
 import uuid
 import calendar,time
+import redis
 from flask import Flask, request, url_for, send_from_directory, render_template, redirect
 from werkzeug import secure_filename
-from flask_redis import FlaskRedis
+#from flask_redis import FlaskRedis
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 REDIS_URL = "redis://:password@localhost:6379/0"
@@ -12,7 +13,8 @@ REDIS_URL = "redis://:password@localhost:6379/0"
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.getcwd()
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-redis_client = FlaskRedis(app)
+#redis_client = FlaskRedis(app)
+r = redis.Redis(host='192.168.100.38', port=6379, db=0)
 
 html = '''
     <!DOCTYPE html>
@@ -94,20 +96,23 @@ def print_req():
             dev_id = 'a46fd75c-a5e0-11e9-86f1-20689d49592c'
             timestamp = calendar.timegm(time.gmtime())
             print('request_id=%s|file_url=%s|color_mode=%s|sides=%s|copys=%s'%(request_id, file_url, color_mode, sides, copys))
-            #request_id = str(uuid.uuid4())
-            #rs.hmset('zs',{'name':'lisi','age':18})
-            #store print request info 
-            redis_client.hmset(request_id, {'file_url':file_url, \
+            #store print request info
+            try:
+                r.hmset(request_id, {'file_url':file_url, \
                                             'color_mode':color_mode, \
                                             'sides':sides, \
                                             'copys':copys, \
                                             'dev_id':dev_id, \
                                             'timestamp':timestamp})
+            except redis.exceptions:
+                print('error hmset failed!')
+                pass
             # insert print request_id into redis list(print_req_list)
-            redis_client.lpush('print_req_list', request_id)
-            #val = 'file_url={}|print_args={}'.format(file_url, print_args)
-            #redis_client.set(key, val)
-            #print('store redis %s=>%s'%(key, val))
+            try:
+                r.lpush('print_req_list', request_id)
+            except redis.exceptions:
+                print('error hmset failed!')
+                pass
 
             return '打印任务提交成功！'
         else:
